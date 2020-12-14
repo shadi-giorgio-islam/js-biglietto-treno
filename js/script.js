d8af8a7e003e27be7db4c356c87c29eac4e60c94
Giorgio <shadigiorgioislam@gmail.com>
2020-12-14 14:17:35 +0100

    END

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var km = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
+var eta = parseInt(prompt('Quanti anni hai?'));
+var prezzo = km * 0.21;
+var sconto;
+
+if (eta < 18){
+  sconto = prezzo * 20 /100;
+  prezzo -= sconto;
+}
+else if(eta > 65){
+  sconto = prezzo * 40 /100;
+  prezzo -= sconto;
+}
+
+document.getElementById('biglietto').innerHTML = 'il prezzo del tuo biglietto è' + ' ' + prezzo + '€';
